a86f5cfe6ed964c7e7e6376c3ecaafba879df8de
Eric-DelGuerra <47738252+eric-delguerra@users.noreply.github.com>
2019-10-29 09:07:23 +0100

    Controller update

--- src/main/java/com/vehiculerental/reservations/ReservationController.java
+++ src/main/java/com/vehiculerental/reservations/ReservationController.java
@@ -2,7 +2,12 @@ package com.vehiculerental.reservations;
 
 import com.vehiculerental.reservations.DAO.ReservationDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
 
 @RestController
 public class ReservationController {
@@ -11,28 +16,33 @@ public class ReservationController {
     private ReservationDao reservationDao;
 
     @GetMapping(value = "/reservations")
-    public Iterable<Reservation> listReservations(){
+    public Iterable<Reservation> listReservations() {
         return reservationDao.findAll();
     }
 
     @GetMapping(value = "/reservation/{id}")
-    public Reservation findReservation(@PathVariable("id") int id){
+    public Reservation findReservation(@PathVariable("id") int id) {
         return reservationDao.findById(id);
     }
 
     @PostMapping(value = "/reservation")
-    public void addReservation(@RequestBody Reservation reservation){
+    public void addReservation(@RequestBody Reservation reservation) {
         reservationDao.save(reservation);
     }
 
     @DeleteMapping(value = "/reservation/{id}")
-    public void deleteReservation(@PathVariable("id") int id){
+    public void deleteReservation(@PathVariable("id") int id) {
         Reservation reservation = reservationDao.findById(id);
         reservationDao.delete(reservation);
     }
 
-    @PutMapping(value = "reservation")
-    public void modifyReservation(@RequestBody Reservation reservation){
+    @PutMapping(value = "/reservation")
+    public void modifyReservation(@RequestBody Reservation reservation) {
         reservationDao.save(reservation);
+    }
+
+    @GetMapping(value = "/datesOfReservation")
+    public List<Reservation> getAllBetweenDates(@RequestParam("endDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date endDate, @RequestParam("startDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date startDate) {
+        return reservationDao.findAllByDateOfBeginningAfterAndDateOfEndBefore(startDate, endDate);
     }
 }
